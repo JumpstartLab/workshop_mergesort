c6fb597564a5662f6d6cd65d3ac8beb8e8c2140e
Chris Morris <chris.morris@livingsocial.com>
2012-10-25 15:41:13 -0500

    latest

--- jasmine/src/MergeSort.js
+++ jasmine/src/MergeSort.js
@@ -1,26 +1,34 @@
 function MergeSort() {
-  
+
 }
 
 MergeSort.prototype = {
   sort:function (list) {
     workArray = [];
-    for (var i = 0; i < list.length; i++) 
-    {
+    for (var i = 0; i < list.length; i++) {
       workArray.push([list[i]])
     }
-    
+
+    var i = 0;
+    var j = Math.min(workArray.length - 1, 1);
+
+    if (workArray[i] > workArray[j]) {
+      var temp = workArray[j];
+      workArray[j] = workArray[i];
+      workArray[i] = temp;
+    }
+
     return this.flatten(workArray);
   },
-  
+
   flatten:function (array) {
-      if (array.length > 0) {
-        return array.reduce(function (a, b) {
-          return a.concat(b);
-        });
-      }
-      else {
-        return array;
-      }
+    if (array.length > 0) {
+      return array.reduce(function (a, b) {
+        return a.concat(b);
+      });
     }
+    else {
+      return array;
+    }
+  }
 }
