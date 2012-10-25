72a8b41800da2b96cbedac3dfa64d7bcdfb716ff
William Davenport <bill.davenport@livingsocial.com>
2012-10-25 16:35:34 -0400

    basic mergesort for two element array

--- jasmine/src/MergeSort.js
+++ jasmine/src/MergeSort.js
@@ -1,9 +1,26 @@
 function MergeSort() {
-
+  
 }
 
 MergeSort.prototype = {
-  sort:function () {
-    return [];
-  }
+  sort:function (list) {
+    workArray = [];
+    for (var i = 0; i < list.length; i++) 
+    {
+      workArray.push([list[i]])
+    }
+    
+    return this.flatten(workArray);
+  },
+  
+  flatten:function (array) {
+      if (array.length > 0) {
+        return array.reduce(function (a, b) {
+          return a.concat(b);
+        });
+      }
+      else {
+        return array;
+      }
+    }
 }
