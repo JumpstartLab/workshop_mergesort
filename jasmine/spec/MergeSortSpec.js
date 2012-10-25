c58d51536739c4d30119d71dfec44de9c4f3e997
Chris Morris <chris.morris@livingsocial.com>
2012-10-25 14:55:33 -0500

    latest

--- jasmine/spec/MergeSortSpec.js
+++ jasmine/spec/MergeSortSpec.js
@@ -2,4 +2,8 @@ describe("MergeSort", function() {
   it("should sort empty list", function(){
     expect(new MergeSort().sort([])).toEqual([]);
   });
+
+  it("should sort single element list", function() {
+    expect(new MergeSort().sort([1])).toEqual([1]);
+  });
 });
