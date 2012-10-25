c0002a532a83153250c0033e9a7e721132016cf7
Chris Morris <chris.morris@livingsocial.com>
2012-10-25 14:59:41 -0500

    latest

--- jasmine/spec/MergeSortSpec.js
+++ jasmine/spec/MergeSortSpec.js
@@ -6,4 +6,12 @@ describe("MergeSort", function() {
   it("should sort single element list", function() {
     expect(new MergeSort().sort([1])).toEqual([1]);
   });
+
+  it("should sort already sorted two element list", function() {
+    expect(new MergeSort().sort([1, 2])).toEqual([1, 2]);
+  });
+
+  it("should sort unsorted two element list", function() {
+    expect(new MergeSort().sort([2, 1])).toEqual([1, 2]);
+  });
 });
