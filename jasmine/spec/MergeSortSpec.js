361946850ed38d44a34b2e89745e005ec37696a9
Chris Morris <chris.morris@livingsocial.com>
2012-10-25 15:42:26 -0500

    latest

--- jasmine/spec/MergeSortSpec.js
+++ jasmine/spec/MergeSortSpec.js
@@ -1,17 +1,21 @@
-describe("MergeSort", function() {
-  it("should sort empty list", function(){
+describe("MergeSort", function () {
+  it("should sort empty list", function () {
     expect(new MergeSort().sort([])).toEqual([]);
   });
 
-  it("should sort single element list", function() {
+  it("should sort single element list", function () {
     expect(new MergeSort().sort([1])).toEqual([1]);
   });
 
-  it("should sort already sorted two element list", function() {
+  it("should sort already sorted two element list", function () {
     expect(new MergeSort().sort([1, 2])).toEqual([1, 2]);
   });
 
-  it("should sort unsorted two element list", function() {
+  it("should sort unsorted two element list", function () {
     expect(new MergeSort().sort([2, 1])).toEqual([1, 2]);
+  });
+
+  it("should sort unsorted three element list", function () {
+    expect(new MergeSort().sort([3, 2, 1])).toEqual([1, 2, 3]);
   });
 });
